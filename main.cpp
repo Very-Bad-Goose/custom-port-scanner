87cfd4237d25803bb21dba37db10ac66c29fce94
Joaquim Pedroza <71528875+Very-Bad-Goose@users.noreply.github.com>
2023-03-08 14:46:06 -0800

    Update main.cpp
    updated and optimized

--- main.cpp
+++ main.cpp
@@ -1,71 +1,106 @@
 #include <iostream>
 #include <string>
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <mutex>
+#include <atomic>
+#include <future>
+#include <sstream>
+#include <stdexcept>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int main()
+constexpr int MAX_THREADS = 256;
+constexpr int TIMEOUT_MS = 100;
+
+std::mutex mutex;
+
+std::atomic_int num_open_ports(0);
+
+std::vector<int> open_ports;
+
+// Scans a single port and adds it to the list of open ports if it's open
+void scan_port(const std::string& ip_addr, int port)
 {
-    WSADATA wsaData;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (result != 0)
-    {
-        std::cerr << "WSAStartup failed: " << result << std::endl;
-        return 1;
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    InetPton(AF_INET, ip_addr.c_str(), &server.sin_addr);
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        return;
     }
 
+    u_long iMode = 1;
+    ioctlsocket(sock, FIONBIO, &iMode);
+
+    int result = connect(sock, reinterpret_cast<sockaddr*>(&server), sizeof(server));
+    if (result == SOCKET_ERROR) {
+        int error = WSAGetLastError();
+        if (error != WSAEWOULDBLOCK && error != WSAECONNREFUSED) {
+            closesocket(sock);
+            return;
+        }
+        fd_set set;
+        FD_ZERO(&set);
+        FD_SET(sock, &set);
+        timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = TIMEOUT_MS * 1000;
+        result = select(0, NULL, &set, NULL, &timeout);
+        if (result <= 0) {
+            closesocket(sock);
+            return;
+        }
+    }
+
+    std::lock_guard<std::mutex> lock(mutex);
+    ++num_open_ports;
+    open_ports.push_back(port);
+
+    closesocket(sock);
+}
+
+int main()
+{
     std::string ip_addr;
     std::cout << "Enter IP address: ";
     std::cin >> ip_addr;
 
-    // Create a TCP socket
-    SOCKET sock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
-    if (sock == INVALID_SOCKET)
-    {
-        std::cerr << "Socket creation failed: " << WSAGetLastError() << std::endl;
-        return 1;
-    }
+    std::vector<std::future<void>> tasks;
 
-    // Prepare sockaddr_in structure
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(ip_addr.c_str());
-
-    // Scan ports from 1 to 65535
-    for (int port = 1; port <= 65535; port++)
-    {
-        // Set port number
-        addr.sin_port = htons(port);
-
-        // Attempt to connect to the remote host
-        if (connect(sock, (SOCKADDR*)&addr, sizeof(addr)) != SOCKET_ERROR)
-        {
-            std::cout << "Port " << port << " is open." << std::endl;
-        }
-
-        // Close socket and reset error code
-        closesocket(sock);
-        WSACleanup();
-
-        result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-        if (result != 0)
-        {
-            std::cerr << "WSAStartup failed: " << result << std::endl;
-            return 1;
-        }
-
-        sock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
-        if (sock == INVALID_SOCKET)
-        {
-            std::cerr << "Socket creation failed: " << WSAGetLastError() << std::endl;
-            return 1;
+    for (int port = 1; port <= 65535; ++port) {
+        tasks.push_back(std::async(std::launch::async, scan_port, ip_addr, port));
+        if (tasks.size() >= MAX_THREADS) {
+            for (auto& task : tasks) {
+                task.get();
+            }
+            tasks.clear();
         }
     }
 
-    // Clean up
-    closesocket(sock);
-    WSACleanup();
+    for (auto& task : tasks) {
+        task.get();
+    }
+
+    if (num_open_ports == 0) {
+        std::cout << "No open ports found." << std::endl;
+    }
+    else {
+        std::sort(open_ports.begin(), open_ports.end());
+        std::stringstream ss;
+        for (int port : open_ports) {
+            ss << port << ", ";
+        }
+        std::string result = ss.str();
+        result = result.substr(0, result.size() - 2);
+        std::cout << "Open ports: " << result << std::endl;
+    }
 
     return 0;
 }
